cd7e0c7bc5df55d46a861a1863605894476ca517
Manas0410 <manasshrivastava0410@gmail.com>
2025-07-25 02:08:39 +0530

    connected with client and fetched users

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery, useMutation, gql } from "@apollo/client";
 
-function App() {
-  const [count, setCount] = useState(0)
+const GET_USERS = gql`
+  query getUsers {
+    getUsers {
+      age
+      id
+      name
+    }
+  }
+`;
+
+const App = () => {
+  const { loading, error, data } = useQuery(GET_USERS);
+  console.log(data, "dfd");
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>User List</h1>
+      {data.getUsers.map((user) => (
+        <div key={user.id}>
+          <h2>{user.name}</h2>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default App
+export default App;
